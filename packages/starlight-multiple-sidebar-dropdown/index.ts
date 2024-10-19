8b993803174ba9ea45d60a822161b1fec7dd8246
trueberryless <trueberryless+github@gmail.com>
2024-10-19 14:02:14 +0200

    try programming select with shadcn

--- packages/starlight-multiple-sidebar-dropdown/index.ts
+++ packages/starlight-multiple-sidebar-dropdown/index.ts
@@ -70,7 +70,7 @@ export default function starlightMultipleSidebarDropdownPlugin(
 
         addIntegration(
           tailwind({
-            applyBaseStyles: false,
+            applyBaseStyles: true,
           }),
         )
       },
